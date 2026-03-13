94ed723d041a7e58b9f80a1f94151ff78fbbb504
Onyx Developer <dev@onyx.mx>
2026-03-13 17:19:35 -0600

    Update SCRIPT_URL to new deployment

--- src/lib/consts.tsx
+++ src/lib/consts.tsx
@@ -1,7 +1,7 @@
 
 
 export const SCRIPT_URL =
-  'https://script.google.com/macros/s/AKfycbwK_T6ifUOLO0f65SRVbHOqZyhKQbXABaPcBTEVC31JrlQoWjzuJQB9oVI4ib6vHNQ8KA/exec';
+  'https://script.google.com/macros/s/AKfycbz7HTsC9FEqbJ0jiMLjk327q4Qo07SWPh2zZzu7V4vjvdqOcc0iwsrtBWWvywRz_Jn5xw/exec';
 
 export const colors = [
   'rgb(0, 0, 0)',
