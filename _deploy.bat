c229e4599d6487e7437d04e7da243bcbdd7b944a
Onyx Developer <dev@onyx.mx>
2026-04-10 12:17:46 -0600

    v1.71.2 - Google Sites compatibility for iFrameViewer

--- _deploy.bat
+++ _deploy.bat
@@ -1,8 +1,9 @@
 @echo off
 echo [1/3] Committing source changes...
 git add -A
-git commit -m "v1.71.1 - Modernize iFrameViewer.html and synchronize with Studio aesthetic"
+git commit -m "v1.71.2 - Google Sites compatibility for iFrameViewer"
 git push origin main
+
 
 
 
