e406c49fbad0a09407bccc5a82d3cd8c32860d60
Onyx Developer <dev@onyx.mx>
2026-07-05 18:23:14 -0600

    Add missing migration strategy 13 to resolve RxDB DM4 error

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -233,7 +233,7 @@ const createDatabase = async () => {
             inventory: {
                 schema: inventorySchema,
                 migrationStrategies: {
-                    1: () => null, 2: () => null, 3: () => null, 4: () => null, 5: () => null, 6: () => null, 7: () => null, 8: () => null, 9: () => null, 10: () => null, 11: (oldDoc) => oldDoc, 12: (oldDoc) => oldDoc,
+                    1: () => null, 2: () => null, 3: () => null, 4: () => null, 5: () => null, 6: () => null, 7: () => null, 8: () => null, 9: () => null, 10: () => null, 11: (oldDoc) => oldDoc, 12: (oldDoc) => oldDoc, 13: (oldDoc) => oldDoc,
                 }
             },
             finance: {
